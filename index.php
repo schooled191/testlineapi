89a00f307ab03c0285435706df497e570e412ba3
schooled191 <31589155+schooled191@users.noreply.github.com>
2017-10-15 20:33:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
-$ekanan = {
-               "firstName": "เอกนันท์",
-               "lastName": "ขันทอง"
-	}
-echo $ekanan;
+$arr  = Array(
+                "firstName"=>"เอกนันท์",
+                "lastName"=>"ขันทอง",
+                "address" =>
+                            Array(
+                                  "streetAddress"=>"พระราม2",
+                                  "city"=>"กรุงเทพฯ",
+                                  "postalCode"=>"10150"
+                                 ),
+                "phoneNumbers"=>
+                            Array(
+                                  "085-123-4567",
+                                  "02-555-4567"
+                                 )
+               );
+ 
+echo json_encode($arr);
