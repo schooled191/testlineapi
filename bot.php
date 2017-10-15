504cccb5683f0fd650b5f4fcd716cf20f24835cc
schooled191 <31589155+schooled191@users.noreply.github.com>
2017-10-15 19:21:18 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,11 +1,10 @@
 <?php
-$access_token = 'OiFfXS/z6c3apamAsO/5EHnQtIbsw104XLcrFtUV+/wwF/3nd7h49zb/MzmR9QVoenYVUbi7ClKuXf/dFK7juWapND6faujmbYJL79hE1n3iJ6h8yWuHNhh3WeoHCe/ou5VnZL2DrK/e8N/M6UkMaAdB04t89/1O/w1cDnyilFU=';
+$access_token = '3fqNoDxOTgjhRbMSekT8Z98ylLrR3scnw3AqtUCzk2uIIwL6RnE93VZEyyUB+XuldePM+OqbZOxltnCGxgiAOc54/ckuYvKmR5gemIn7UChCoRFG0AOJg14SzdPxhaYJErJbivTQ3JaHKX7zHidyMAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
-$response = "":
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
@@ -16,23 +15,6 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-			$curl = curl_init();
-
-			curl_setopt_array($curl, array(
-			  CURLOPT_URL => "http://localhost/work/OCRBot/FormBotToserver.php",
-			  CURLOPT_RETURNTRANSFER => true,
-			  CURLOPT_ENCODING => "",
-			  CURLOPT_MAXREDIRS => 10,
-			  CURLOPT_TIMEOUT => 30,
-			  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			  CURLOPT_CUSTOMREQUEST => "GET",
-			  CURLOPT_HTTPHEADER => array(
-			    "cache-control: no-cache",
-			    "postman-token: 5cfa0d47-cfdf-2664-cc11-a67457b6e15a"
-			  ),
-			));
-
-			$response = curl_exec($curl);
 
 			// Build message to reply back
 			$messages = [
@@ -44,7 +26,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$response],
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
@@ -92,4 +74,4 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "$response";
+echo "OK";
