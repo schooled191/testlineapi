559b220a2d59e7c7fadf649373cd375679ec2c53
schooled191 <31589155+schooled191@users.noreply.github.com>
2017-10-15 20:44:07 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,15 +1,28 @@
 <?php
-$access_token = '3fqNoDxOTgjhRbMSekT8Z98ylLrR3scnw3AqtUCzk2uIIwL6RnE93VZEyyUB+XuldePM+OqbZOxltnCGxgiAOc54/ckuYvKmR5gemIn7UChCoRFG0AOJg14SzdPxhaYJErJbivTQ3JaHKX7zHidyMAdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v1/oauth/verify';
+$curl = curl_init();
 
-$headers = array('Authorization: Bearer ' . $access_token);
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://fast-cove-30263.herokuapp.com/index.php",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "cache-control: no-cache",
+    "postman-token: 282453ec-7d28-a55d-76d8-6ebd100f5e63"
+  ),
+));
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
+$response = curl_exec($curl);
+$err = curl_error($curl);
 
-echo $result;
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
