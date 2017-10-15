0366bc78ec7d0db738ae42e7d2d7eedccdc86298
schooled191 <31589155+schooled191@users.noreply.github.com>
2017-10-15 22:59:47 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,7 +12,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'image') {
 			// Get text sent
-			$text = $event['message']['image'];
+			$text = $event['message']['type'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
